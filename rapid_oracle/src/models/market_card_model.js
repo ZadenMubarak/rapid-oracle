320383c6b819fe19aaa7ca511f3c76a52a2f76e5
ZadenMubarak <zaden.ngobeni@gmail.com>
2023-11-28 14:46:15 +0200

    aded to the models

--- rapid_oracle/src/models/market_card_model.js
+++ rapid_oracle/src/models/market_card_model.js
@@ -1,10 +1,82 @@
 export default market_car = [
     {
-        image: "",
-        title: "",
-        author: "",
-        long_details: "",
-        short_details: "",
-        
-    }
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+
+    // //////////////////////////////////////////////////
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+
+    {
+        image: "https://cdn.britannica.com/88/62088-004-8245C6C2/graph-curve-calculus-help-steps-x-shape.jpg?w=400&h=300&c=crop",
+        title: "Title",
+        author: "Author",
+        long_details: "This will be the long details of the function",
+        short_details: "shrt details",
+
+    },
+    ///////////////////////////////////////////////
+
+
+    
+
+
 ]
